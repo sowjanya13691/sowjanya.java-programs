4dc6c2fb03c2348ceabac2a834e1e25b3b5a66df
sowjanya13691 <sowjanya13691@gmail.com>
2025-08-19 14:14:29 +0530

    Add files via upload

---
+++ mergesort.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class mergesort {
+    public static void merge(int[] array, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; ++i)
+            L[i] = array[left + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = array[mid + 1 + j];
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            merge(array, left, mid, right);
+        }
+    }
+    public static void printArray(int[] array) {
+        for (int value : array)
+            System.out.print(value + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++)
+            array[i] = scanner.nextInt();
+        System.out.println("Original array:");
+        printArray(array);
+        mergeSort(array, 0, array.length - 1);
+        System.out.println("Sorted array:");
+        printArray(array);
+        scanner.close();
+    }
+}
